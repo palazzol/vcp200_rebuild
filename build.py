52ea120940a7440a0c121b169118e1189738afac
Frank Palazzolo <frank@avoidspikes.com>
2025-04-26 22:30:12 -0400

    Add support for Linux

--- build.py
+++ build.py
@@ -1,15 +1,21 @@
 import os, sys
+import platform
+
+PLATFORM = platform.system()
 
 # Assemble
-if os.system('tools\\as6804.exe -o -p -s -l vcp200.asm') != 0:
+cmd = os.path.join('tools',PLATFORM,'as6804 -o -p -s -l vcp200.asm')
+if os.system(cmd) != 0:
     print('Error during Assembly')
     sys.exit(-1)
 # Link
-if os.system('tools\\aslink.exe -n -m -u -s vcp200.rel') != 0:
+cmd = os.path.join('tools',PLATFORM,'aslink -n -m -u -s vcp200.rel')
+if os.system(cmd) != 0:
     print('Error during Linking')
     sys.exit(-1)
 # Generate Binary
-if os.system('tools\\srec2bin.exe -q -o 0x800 -a 0x0800 -f 00 vcp200.s19 vcp200.bin') != 0:
+cmd = os.path.join('tools',PLATFORM,'srec2bin -q -o 0x800 -a 0x0800 -f 00 vcp200.s19 vcp200.bin')
+if os.system(cmd) != 0:
     print('Error during Binary Generation')
     sys.exit(-1)
 # Compare with reference
